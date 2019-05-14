c09bdb4344ce18fb125d565dd23c861fe6bce1c0
Ahmed T. Youssef <xmonader@gmail.com>
2019-05-14 14:48:11 +0200

    Add property testing

---
+++ tests/test_properties.py
@@ -0,0 +1,26 @@
+from hypothesis import given
+from hypothesis.strategies import text
+import nacl.utils
+import nacl.signing
+from nacl.public import PrivateKey, Box
+from nacl.secret import SecretBox
+import os
+from secretconf import *
+
+USER = "ahmed"
+PASSWORD = "weakpassword"
+APPNAME = 'twitter_app1'
+
+sk = nacl.signing.SigningKey.generate()
+pk = sk.verify_key
+hashedsk = hash32(sk.encode())
+
+hsk = PrivateKey(hashedsk)
+hpk = hsk.public_key
+box = Box(hsk, hpk)
+
+
+@given(text())
+def test_decode_inverts_encode(s):
+    pass
+    assert decrypt(encrypt(s, box), box) == s
