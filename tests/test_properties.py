e011f00b99ed1b1ff64d9672ce155dba240ea886
xmonader <xmonader@gmail.com>
2019-07-09 13:58:57 +0200

    update hypthesis tests

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -1,9 +1,7 @@
 from hypothesis import given
 from hypothesis.strategies import text
 import nacl.utils
-import nacl.signing
 from nacl.public import PrivateKey, Box
-from nacl.secret import SecretBox
 import os
 from secretconf import *
 
@@ -11,11 +9,7 @@ USER = "ahmed"
 PASSWORD = "weakpassword"
 APPNAME = 'twitter_app1'
 
-sk = nacl.signing.SigningKey.generate()
-pk = sk.verify_key
-hashedsk = hash32(sk.encode())
-
-hsk = PrivateKey(hashedsk)
+hsk = PrivateKey.generate()
 hpk = hsk.public_key
 box = Box(hsk, hpk)
 
